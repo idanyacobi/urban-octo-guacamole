c59673b6c0bf8d021672dfe55188203e060bd2ee
avrni <avr.nir@gmail.com>
2017-04-04 16:13:57 +0300

    Adding Canny.

--- app/src/main/java/com/example/idan/urban_octo_guacamole/MainActivity.java
+++ app/src/main/java/com/example/idan/urban_octo_guacamole/MainActivity.java
@@ -6,18 +6,44 @@ import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.JavaCameraView;
+import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2{
 
     private static final String TAG = "MainActivity";
+    JavaCameraView javaCameraView;
+    Mat mRgba,imgGray,imgCanny;
+    BaseLoaderCallback nLoadCallBack = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch(status){
+                case BaseLoaderCallback.SUCCESS:{
+                    javaCameraView.enableView();
+                    break;
+                }
+                default:
+                    super.onManagerConnected(status);
+                    break;
+            }
+
+
+        }
+    };
 
     static {
         if (!OpenCVLoader.initDebug()) {
@@ -28,32 +54,82 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    Button button_take_img;
+//    Button button_take_img;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        button_take_img = (Button) findViewById(R.id.button);
+        javaCameraView = (JavaCameraView)findViewById(R.id.java_camera_view);
+        javaCameraView.setVisibility(SurfaceView.VISIBLE);
+        javaCameraView.setCvCameraViewListener(this);
+//        button_take_img = (Button) findViewById(R.id.button);
     }
 
-    static final int REQUEST_IMAGE_CAPTURE = 1;
+    @Override
+    protected void onPause(){
+        super.onPause();
+        if(javaCameraView != null)
+            javaCameraView.disableView();;
 
-    public void dispatchTakePictureIntent(View v) {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        if(javaCameraView != null)
+            javaCameraView.disableView();
+    }
+    @Override
+    protected void onResume(){
+        super.onResume();
+        if (!OpenCVLoader.initDebug()) {
+            Log.d(TAG, "open cv not loaded");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_2_0,this, nLoadCallBack);
+        }
+        else {
+            Log.d(TAG, "open cv loaded");
+            nLoadCallBack.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+
         }
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            ImageView mImageView = (ImageView) findViewById(R.id.imageView);
-            mImageView.setImageBitmap(imageBitmap);
-        }
+    public void onCameraViewStarted(int width, int height) {
+        mRgba = new Mat(height, width, CvType.CV_8UC4);
+        imgGray = new Mat(height, width, CvType.CV_8UC1);
+        imgCanny = new Mat(height, width, CvType.CV_8UC1);
     }
+
+    @Override
+    public void onCameraViewStopped() {
+        mRgba.release();
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        mRgba = inputFrame.rgba();
+        Imgproc.cvtColor(mRgba, imgGray, Imgproc.COLOR_RGB2GRAY);
+        Imgproc.Canny(imgGray,imgCanny,50,150);
+        return imgCanny;
+    }
+
+//    static final int REQUEST_IMAGE_CAPTURE = 1;
+//
+//    public void dispatchTakePictureIntent(View v) {
+//        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+//            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+//        }
+//    }
+//
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+//            Bundle extras = data.getExtras();
+//            Bitmap imageBitmap = (Bitmap) extras.get("data");
+//            ImageView mImageView = (ImageView) findViewById(R.id.imageView);
+//            mImageView.setImageBitmap(imageBitmap);
+//        }
+//    }
 }
