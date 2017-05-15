c42b6ba558deb08938526b955462699e9c57ba58
Iris Altman <iraltman@cisco.com>
2017-05-15 18:08:20 +0300

    change PATCH sizes from private to public

--- app/src/main/java/com/example/idan/urban_octo_guacamole/Settings.java
+++ app/src/main/java/com/example/idan/urban_octo_guacamole/Settings.java
@@ -4,9 +4,9 @@ import org.opencv.core.Size;
 
 class Settings {
 
-    private static final int PATCH_WIDTH = 8;
-    private static final int PATCH_HEIGHT = 8;
-    public static final Size PATCH_SIZE = new Size(PATCH_WIDTH, PATCH_HEIGHT);
+    static final int PATCH_WIDTH = 8;
+    static final int PATCH_HEIGHT = 8;
+    static final Size PATCH_SIZE = new Size(PATCH_WIDTH, PATCH_HEIGHT);
     static final int DESC_LENGTH = 64;
 
 }
