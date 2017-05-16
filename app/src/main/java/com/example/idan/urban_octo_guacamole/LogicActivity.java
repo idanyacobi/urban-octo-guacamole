216064bbbae37edce4bb573717d0e40e111c7b0b
Iris Altman <iraltman@cisco.com>
2017-05-16 17:47:54 +0300

    add face 22

--- app/src/main/java/com/example/idan/urban_octo_guacamole/LogicActivity.java
+++ app/src/main/java/com/example/idan/urban_octo_guacamole/LogicActivity.java
@@ -132,7 +132,8 @@ public class LogicActivity extends AppCompatActivity {
         Integer upper_row = row + ENV_SIZE + PATCH_SIZE;
         Integer lower_row = row - ENV_SIZE;
 
-        String query = String.format("select * from descriptors where col > %d and col < %d and row > %d and row < %d;", lower_col, upper_col, lower_row, upper_row);
+//        String query = String.format("select * from descriptors where col > %d and col < %d and row > %d and row < %d;", lower_col, upper_col, lower_row, upper_row);
+        String query = String.format("select * from descriptors where col == %d and row == %d", col, row);
         List<Descriptor> query_res = databaseAccess.exeDescriptorsQuery(query);
 
         return query_res;
