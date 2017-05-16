f39cda24a4b9d2963221b76e41675bcd18f7f7f4
idan <idan2412@gmail.com>
2017-05-16 15:44:56 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/java/com/example/idan/urban_octo_guacamole/Descriptor.java
    #	app/src/main/java/com/example/idan/urban_octo_guacamole/Settings.java

---
+++ app/src/main/java/com/example/idan/urban_octo_guacamole/utils.java
@@ -0,0 +1,34 @@
+package com.example.idan.urban_octo_guacamole;
+
+import android.graphics.Bitmap;
+import android.util.Log;
+
+import org.opencv.android.Utils;
+import org.opencv.core.CvException;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+
+/**
+ * Created by idan on 16/05/2017.
+ */
+
+public class utils {
+    public static Bitmap mat2bmp(Mat m) {
+        Bitmap bmp = null;
+        Mat tmp = new Mat (m.rows(), m.cols(), CvType.CV_8U, new Scalar(4));
+
+        try {
+            //Imgproc.cvtColor(seedsImage, tmp, Imgproc.COLOR_RGB2BGRA);
+            Imgproc.cvtColor(m, tmp, Imgproc.COLOR_GRAY2RGBA, 4);
+            bmp = Bitmap.createBitmap(tmp.cols(), tmp.rows(), Bitmap.Config.ARGB_8888);
+            Utils.matToBitmap(tmp, bmp);
+        }
+        catch (CvException e){
+            Log.d("Exception",e.getMessage());
+        }
+
+        return bmp;
+    }
+}
