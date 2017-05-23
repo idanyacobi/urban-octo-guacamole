d00e5331317d09e0791c5c547f0eeacece4dd892
irisalt21 <1qazxcvb>
2017-05-23 16:15:48 +0300

    refactor

--- app/src/main/java/com/example/idan/urban_octo_guacamole/DepthConstructor.java
+++ app/src/main/java/com/example/idan/urban_octo_guacamole/DepthConstructor.java
@@ -7,19 +7,16 @@ import org.opencv.core.Scalar;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Created by idan on 15/05/2017.
- */
 
-public class DepthConstructor {
+class DepthConstructor {
 
-    ArrayList<DepthPatch> _patches;
+    private ArrayList<DepthPatch> _patches;
 
-    public DepthConstructor(ArrayList patches) {
+    DepthConstructor(ArrayList patches) {
         _patches = patches;
     }
 
-    public Mat Construct() {
+    Mat Construct() {
         Mat depth = Mat.zeros(Settings.IMAGE_SIZE, Settings.IMAGE_CVTYPE);
         for (DepthPatch dp : _patches) {
             if ((dp.getCol() % Settings.PATCH_SIZE != 0) || (dp.getRow() % Settings.PATCH_SIZE != 0)){
