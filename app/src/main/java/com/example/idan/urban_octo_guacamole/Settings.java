1cf21f8b4ec17782594dcbbc6a3fcb46451f8e86
irisalt21 <1qazxcvb>
2017-05-23 16:19:20 +0300

    refactor + change size

--- app/src/main/java/com/example/idan/urban_octo_guacamole/Settings.java
+++ app/src/main/java/com/example/idan/urban_octo_guacamole/Settings.java
@@ -5,17 +5,20 @@ import static org.opencv.core.CvType.CV_8UC1;
 
 class Settings {
 
-    static final int ENV_SIZE = 8;
+    static final int ENV_SIZE = 1;
     static final int IMAGE_CVTYPE = CV_8UC1;
 
-    static final int WIDTH_SIZE = 256;
-    static final int HEIGHT_SIZE = 256;
+    static final int WIDTH_SIZE = 128;
+    static final int HEIGHT_SIZE = 128;
     static final int WINDOW_SIZE = 16;
-    static final int STEP_OVERLAP = 1;
-    static final int BLOCK = 8;
-    static final int CELL_SIZE = 2;
-    static final int PADDING_SIZE = 0;
+    static final int BLOCK_SIZE = 16;
+    static final int CELL_SIZE = 8;
     static final int PATCH_SIZE = 16;
+
+    static final int STEP_OVERLAP = 1;
+    static final int PADDING_SIZE = 0;
     static final int OVERLAP_SIZE = PATCH_SIZE / STEP_OVERLAP;
     static final Size IMAGE_SIZE = new Size(WIDTH_SIZE, HEIGHT_SIZE);
+
+    static final int K_NEAREST = 3;
 }
